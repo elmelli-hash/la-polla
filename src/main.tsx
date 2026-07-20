f2eb82c367c21b2125c8920b646ca4c7c596ef8b
elmelli-hash <sdblasetti@gmail.com>
2026-07-20 09:41:37 -0300

    Mostrar errores en celular

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,45 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+window.addEventListener('error', (event) => {
+  document.body.innerHTML = `
+    <div style="
+      background:#111;
+      color:#ff6b6b;
+      padding:20px;
+      min-height:100vh;
+      font-family:monospace;
+      white-space:pre-wrap;
+    ">
+      ERROR JAVASCRIPT
+
+      ${event.message}
+
+      Archivo:
+      ${event.filename}
+
+      Línea:
+      ${event.lineno}:${event.colno}
+    </div>
+  `
+})
+
+window.addEventListener('unhandledrejection', (event) => {
+  document.body.innerHTML = `
+    <div style="
+      background:#111;
+      color:#ff6b6b;
+      padding:20px;
+      min-height:100vh;
+      font-family:monospace;
+      white-space:pre-wrap;
+    ">
+      PROMESA RECHAZADA
+
+      ${String(event.reason?.stack || event.reason)}
+    </div>
+  `
+})
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
